d11cf5a04ae39524449d57d0fcef45b6268f21d9
Megan Brown <31446963+megan-davi@users.noreply.github.com>
2018-09-21 15:06:00 -0500

    Finished fib sequence file

--- fibonacciSequence.py
+++ fibonacciSequence.py
@@ -7,19 +7,21 @@ show how dynamic programming improves the performance.
 What is the complexity of the algorithm with and without
 dynamic programming?
 '''
-    
+
 class Fibonacci:
-    
+
     def __init__(self):
         "Constructor"
         print("Fibonacci object created.")
         print()
+        self.iterative_count = 0
+        self.recursive_count = 0
 
-   
     def recursive(self, n):
         "Determine fibonacci sequence recursively"
-        if n == 0:      # n represents number of elements for fib. sequence
-            return 0   
+        self.recursive_count += 1
+        if n == 0:  # n represents number of elements for fib. sequence
+            return 0
         elif n == 1:
             return 1
         else:
@@ -29,8 +31,10 @@ class Fibonacci:
         "Determine fibonacci sequence iteratively"
         a, b = 0, 1
         for i in range(0, n):
+            self.iterative_count += 1
             a, b = b, a + b
         return a
+
 
 n = 10
 f = Fibonacci()
@@ -38,18 +42,17 @@ f = Fibonacci()
 print("Printed recursively")
 for i in range(n):
     print(f.recursive(i))
-    
+
 print()
 print("Printed iteratively")
 for i in range(n):
     print(f.iterative(i))
 
+print()
+print("Number of iterations in recursive function: " + str(f.recursive_count))
+print("Number of iterations in iterative function: " + str(f.iterative_count))
+
 '''
-Complexity of dynamic is O(2^n)
+Complexity of dynamic is O(2^n) (exponential)
 Complexity of non-dynamic is O(n) (linear)
 '''
-
-
-
-
-
