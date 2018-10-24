f416718f756217f1f9c82aeb5bf2b5b494dcad8f
Megan Brown <31446963+megan-davi@users.noreply.github.com>
2018-10-24 17:35:03 -0500

    Added tree

--- morseCode.py
+++ morseCode.py
@@ -1,7 +1,9 @@
 """
-Prompt: use a tree to decode a string of morse code
+Prompt: use a binary tree to decode a string of morse code
 """
 
+import re
+import itertools
 class BinaryTree:
     def __init__(self,rootObj):
         self.key = rootObj
@@ -38,26 +40,109 @@ class BinaryTree:
         return self.key
 
 
-
+# build the tree
 tree = BinaryTree("")
 tree.insertLeft("E")
 tree.insertRight("T")
-tree.getRightChild
-tree.insertLeft("I")
-tree.insertRight("A")
+tree.getLeftChild().insertLeft("I")
+tree.getLeftChild().insertRight("A")
+tree.getLeftChild().getLeftChild().insertLeft("S")
+tree.getLeftChild().getLeftChild().insertRight("U")
+tree.getLeftChild().getLeftChild().getLeftChild().insertLeft("H")
+tree.getLeftChild().getLeftChild().getLeftChild().insertRight("V")
+tree.getLeftChild().getLeftChild().getRightChild().insertLeft("F")
+tree.getLeftChild().getRightChild().insertLeft("R")
+tree.getLeftChild().getRightChild().insertRight("W")
+tree.getLeftChild().getRightChild().getLeftChild().insertLeft("L")
+tree.getLeftChild().getRightChild().getRightChild().insertLeft("P")
+tree.getLeftChild().getRightChild().getRightChild().insertRight("J")
+tree.getRightChild().insertLeft("N")
+tree.getRightChild().insertRight("M")
+tree.getRightChild().getLeftChild().insertLeft("D")
+tree.getRightChild().getLeftChild().insertRight("K")
+tree.getRightChild().getLeftChild().getLeftChild().insertLeft("B")
+tree.getRightChild().getLeftChild().getLeftChild().insertRight("X")
+tree.getRightChild().getLeftChild().getRightChild().insertLeft("C")
+tree.getRightChild().getLeftChild().getRightChild().insertRight("Y")
+tree.getRightChild().getRightChild().insertLeft("G")
+tree.getRightChild().getRightChild().insertRight("O")
+tree.getRightChild().getRightChild().getLeftChild().insertLeft("Z")
+tree.getRightChild().getRightChild().getLeftChild().insertRight("Q")
+
+decoded = " "
+fileobj = open("in.txt", 'r')
+
+lines = fileobj.readlines()
+letters = [elem.rstrip('\n').split(" ") for elem in lines]
+letters = sum(word, [])
+
+print(word)
+#print(string)
+
+for letter in letters:
+# decode(letter)
+
     
-string = "-- --- .-. ... .     -.-. --- -.. ."
+    if character == ".":
+        decoded += tree.getLeftChild().getRootVal()
+    elif character == "..":
+        decoded += tree.getLeftChild().getLeftChild().getRootVal()
+    elif character == "...":
+        decoded += tree.getLeftChild().getLeftChild().getLeftChild().getRootVal()
+    elif character == "....":
+        decoded += tree.getLeftChild().getLeftChild().getLeftChild().getLeftChild().getRootVal()
+    elif character == "...-":
+        decoded += tree.getLeftChild().getLeftChild().getLeftChild().getRightChild().getRootVal()
+    elif character == "..-.":
+        decoded += tree.getLeftChild().getLeftChild().getRightChild().getLeftChild().getRootVal()
+    elif character == "..-":
+        decoded += tree.getLeftChild().getLeftChild().getRightChild().getRootVal()
+    elif character == ".-":
+        decoded += tree.getLeftChild().getRightChild().getRootVal()
+    elif character == ".-.":
+        decoded += tree.getLeftChild().getRightChild().getLeftChild().getRootVal()
+    elif character == ".-..":
+        decoded += tree.getLeftChild().getRightChild().getLeftChild().getLeftChild().getRootVal()
+    elif character == ".--":
+        decoded += tree.getLeftChild().getRightChild().getRightChild().getRootVal()
+    elif character == ".--.":
+        decoded += tree.getLeftChild().getRightChild().getRightChild().getLeftChild().getRootVal()
+    elif character == ".---":
+        decoded += tree.getLeftChild().getRightChild().getRightChild().getRightChild().getRootVal()
+    elif character == "-":
+        decoded += tree.getRightChild().getRootVal()
+    elif character == "-.":
+        decoded += tree.getRightChild().getLeftChild().getRootVal()
+    elif character == "-..":
+        decoded += tree.getRightChild().getLeftChild().getLeftChild().getRootVal()
+    elif character == "-...":
+        decoded += tree.getRightChild().getLeftChild().getLeftChild().getLeftChild().getRootVal()
+    elif character == "-..-":
+        decoded += tree.getRightChild().getLeftChild().getLeftChild().getRightChild().getRootVal()
+    elif character == "-.-":
+        decoded += tree.getRightChild().getLeftChild().getRightChild().getRootVal()
+    elif character == "-.-.":
+        decoded += tree.getRightChild().getLeftChild().getRightChild().getLeftChild().getRootVal()        
+    elif character == "-.--":
+        decoded += tree.getRightChild().getLeftChild().getRightChild().getRightChild().getRootVal()
+    elif character == "--":
+        decoded += tree.getRightChild().getRightChild().getRootVal() 
+    elif character == "--.":
+        decoded += tree.getRightChild().getRightChild().getLeftChild().getRootVal()
+    elif character == "---":
+        decoded += tree.getRightChild().getRightChild().getRightChild().getRootVal()
+    elif character == "--..":
+        decoded += tree.getRightChild().getRightChild().getLeftChild().getLeftChild().getRootVal()
+    elif character == "--.-":
+        decoded += tree.getRightChild().getRightChild().getLeftChild().getRightChild().getRootVal()
+    elif character == "":
+        decoded += " "
+    elif character == "\n":
+        decoded += "\n"
 
 
-print(tree.getLeftChild().getRootVal())
-print(tree.getRightChild().getRootVal())
+print()
+print (re.sub(' +', ' ',decoded))
 
-##print(r.getRootVal())
-##print(r.getLeftChild())
-##r.insertLeft('b')
-##print(r.getLeftChild().getRootVal())
-##r.insertRight('c')
-##print(r.getRightChild().getRootVal())
-##r.getRightChild().setRootVal('hello')
-##print(r.getRightChild().getRootVal())
+
 
